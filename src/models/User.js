257539732ff69d0eddd2cfb701699b69de36dbe1
Lup1in <gabrielcunhaleite@hotmail.com>
2023-10-20 13:55:52 -0300

    users added

---
+++ src/models/User.js
@@ -0,0 +1,51 @@
+import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 20],
+            msg: 'Campo Nome deve ter entre 3 e 20 caracteres.',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email já existente',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Email inválido.',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 25],
+            msg: 'Campo Senha deve ter entre 6 e 25 caracteres.',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
+    return this;
+  }
+}
