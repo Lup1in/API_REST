7c754941380f6f76825449a8a018f510ccba238f
Lup1in <gabrielcunhaleite@hotmail.com>
2023-10-25 20:28:42 -0300

    added: delete, update, show, token, tokenExpiration

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,11 +4,78 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({ errors: e.errors.map((err) => err.message) });
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Missing ID, (id não enviado, id faltando).'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User does not exist, (usuário não existente).'],
+        });
+      }
+
+      const newData = await user.update(req.body);
+      return res.json(newData);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Missing ID, (id não enviado, id faltando).'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User does not exist, (usuário não existente).'],
+        });
+      }
+      await user.destroy();
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
     }
   }
 }
-
 export default new UserController();
