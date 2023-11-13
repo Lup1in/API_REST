26eee7210a751959018865ac25a21a3f137bdd03
Lup1in <gabrielcunhaleite@hotmail.com>
2023-11-13 13:43:46 -0300

    Deploy API pt1

---
+++ dist/routes/foto.js
@@ -0,0 +1,8 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
+var _FotoController = require('../controllers/FotoController'); var _FotoController2 = _interopRequireDefault(_FotoController);
+// import loginRequired from '../middlewares/loginRequired';
+
+const router = new (0, _express.Router)();
+
+router.post('/', _FotoController2.default.store);
+exports. default = router;
--- src/app.js
+++ src/app.js
@@ -3,14 +3,14 @@ import { resolve } from 'path';
 
 dotenv.config();
 
-import './src/database';
+import './database';
 
 import express from 'express';
-import home from './src/routes/home';
-import user from './src/routes/user';
-import tokens from './src/routes/token';
-import aluno from './src/routes/aluno';
-import foto from './src/routes/foto';
+import home from './routes/home';
+import user from './routes/user';
+import tokens from './routes/token';
+import aluno from './routes/aluno';
+import foto from './routes/foto';
 
 class App {
   constructor() {
